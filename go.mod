14555b0d22e565dd64d07e6b2e34be0c05b3399a
Antonio Papa <antoniopapa1991@gmail.com>
2021-06-01 14:12:04 +0200

    first commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module chat
+
+go 1.16
+
+require (
+	github.com/gofiber/fiber/v2 v2.10.0
+	github.com/pusher/pusher-http-go v4.0.1+incompatible
+	github.com/stretchr/testify v1.7.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/pusher/pusher-http-go"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Use(cors.New())
+
+	pusherClient := pusher.Client{
+		AppID:   "",
+		Key:     "",
+		Secret:  "",
+		Cluster: "",
+		Secure:  true,
+	}
+
+	app.Post("/api/messages", func(c *fiber.Ctx) error {
+		var data map[string]string
+
+		if err := c.BodyParser(&data); err != nil {
+			return err
+		}
+
+		pusherClient.Trigger("chat", "message", data)
+
+		return c.JSON([]string{})
+	})
+
+	app.Listen(":8000")
+}
